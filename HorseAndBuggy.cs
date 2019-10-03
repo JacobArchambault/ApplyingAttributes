47ea106e035ef30a89c24e6a3cd63add0b221822
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-03 08:57:17 -0400

    Add project files.

---
+++ HorseAndBuggy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplyingAttributes
+{
+    [Serializable, Obsolete("Use another vehicle!")]
+    class HorseAndBuggy
+    {
+    }
+}
---
+++ Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplyingAttributes
+{
+    // This class can be saved to disk.
+    [Serializable]
+    class Motorcycle
+    {
+        // However, this field will not be persisted.
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+        // These fields are still serializable.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+}
